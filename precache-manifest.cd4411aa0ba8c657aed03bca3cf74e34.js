d6d1e0c00efca0f6549252720f49e26630e996d2
arnaudruffin <arnaudruffin@users.noreply.github.com>
2020-01-29 16:36:55 +0000

    Automated deployment: Wed Jan 29 16:36:55 UTC 2020 9529330f13981ed37668905d44be7f8733334ebf

---
+++ precache-manifest.cd4411aa0ba8c657aed03bca3cf74e34.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a8b38bd07718170f8e4",
+    "url": "/jive-community-promotion/css/app.be502927.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/jive-community-promotion/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "05db20a4a090789b1552c017d15caafa",
+    "url": "/jive-community-promotion/index.html"
+  },
+  {
+    "revision": "a87a44f1c9f68554f7b5",
+    "url": "/jive-community-promotion/js/about.bef4e6f9.js"
+  },
+  {
+    "revision": "2a8b38bd07718170f8e4",
+    "url": "/jive-community-promotion/js/app.bfa953de.js"
+  },
+  {
+    "revision": "7df3e163a764ff96d88a",
+    "url": "/jive-community-promotion/js/chunk-vendors.d601a8f0.js"
+  },
+  {
+    "revision": "005a11dbc266d21d113e07f21092d0fc",
+    "url": "/jive-community-promotion/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/jive-community-promotion/robots.txt"
+  }
+]);
