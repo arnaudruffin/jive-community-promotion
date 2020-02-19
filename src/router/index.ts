493cc07f183e7b6cee4203699e96a81eefe11511
Ruffin Arnaud <arnaud.ruffin@orange.com>
2020-02-19 16:31:29 +0100

    add static slideshow

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,7 +7,8 @@ Vue.use(VueRouter);
 const routes = [
     {path: '/', name: 'home', component: Home},
     {path: '/tag/:id/trombi', component: () => import('../views/Trombi.vue')},
-    {path: '/tag/:id/stats', component: () => import('../views/Stats.vue')}
+    {path: '/tag/:id/stats', component: () => import('../views/Stats.vue')},
+    {path: '/tag/:id/slideshow', component: () => import('../views/Slideshow.vue')}
 ];
 
 const router = new VueRouter({
---
+++ src/views/Slideshow.vue
@@ -0,0 +1,77 @@
+<template>
+    <div id="dynamic-component-demo" class="demo">
+        <button
+                v-for="tab in tabs"
+                v-bind:key="tab"
+                v-bind:class="['tab-button', { active: currentTab === tab }]"
+                v-on:click="currentTab = tab"
+        >
+            {{ tab }}
+        </button>
+
+        <component v-bind:is="currentTabComponent" class="tab"></component>
+    </div>
+</template>
+
+
+<script lang="ts">
+
+
+    import {Component, Vue} from "vue-property-decorator";
+    import HelloWorld from "@/components/HelloWorld.vue";
+    import IdCardCollection from "@/components/IdCardCollection.vue";
+    import Stats from "@/views/Stats.vue";
+
+
+
+
+
+    @Component({
+        components: {HelloWorld,IdCardCollection,Stats}
+    })
+    export default class Slideshow extends Vue {
+
+
+        currentTab = "home";
+        tabs = ["home", "trombi", "stats"];
+
+        get currentTabComponent() {
+            switch (this.currentTab) {
+                case "trombi":
+                    return "IdCardCollection";
+                case "stats":
+                    return "Stats";
+                default:
+                    return "HelloWorld";
+            }
+
+        }
+
+
+
+    }
+
+
+</script>
+<style>
+    .tab-button {
+        padding: 6px 10px;
+        border-top-left-radius: 3px;
+        border-top-right-radius: 3px;
+        border: 1px solid #ccc;
+        cursor: pointer;
+        background: #f0f0f0;
+        margin-bottom: -1px;
+        margin-right: -1px;
+    }
+    .tab-button:hover {
+        background: #e0e0e0;
+    }
+    .tab-button.active {
+        background: #e0e0e0;
+    }
+    .tab {
+        border: 1px solid #ccc;
+        padding: 10px;
+    }
+</style>
